dca809ceb26e4768e3709b2db978e5c833e5caf2
Luis Saavedra <nriqu322@gmail.com>
2020-06-15 14:20:58 -0600

    add Book function and html skeleton

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ function Book(title, author, pages, read) {
   this.author = author
   this.pages = pages
   this.read = read
+  this.check = function () {
+    if(read == true){
+      return 'read';
+    } else {
+      return "didn't read yet"
+    }
+    }
   this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${read}`
+    return `${title} by ${author}, ${pages} pages, ${this.check()}`
   }
 }
 
@@ -14,5 +21,9 @@ function addBookToLibrary() {
 
 }
 
-const book1 = new Book('La Odisea', 'anonymus', 243, 'not read')
-console.log(book1.info());
+
+let tag_id = document.getElementById('books');
+tag_id.innerHTML('HTML string');
+
+// const book1 = new Book('La Odisea', 'anonymus', 243, 'not read')
+// console.log(book1.info());
