5cd827298a14907f0654472197d10e45e64b469a
Luis Saavedra <nriqu322@gmail.com>
2020-06-15 16:52:01 -0600

    add render funtion

--- script.js
+++ script.js
@@ -1,32 +1,68 @@
-let myLibrary = []
-
-render(<p>Hello</p>, document.getElementById('books'));
+let myLibrary = [];
 
 function Book(title, author, pages, read = false, data) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.data = data
-  this.check = function () {
-    if(read == true){
-      return 'read';
-    } else {
-      return "didn't read yet"
-    }
-    }
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${this.check()}`
-  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.data = data;
+  // this.check = function () {
+  //   if(read == true){
+  //     return 'read';
+  //   } else {
+  //     return "didn't read yet";
+  //   }
+  //   }
+  // this.iconst bookList = document.createElement('div');nfo = function() {
+  //   return `${title} by ${author}, ${pages} pages, ${this.check()}`;
+  // }
 }
 
 function addBookToLibrary(obj) {
   myLibrary.push(obj);
 }
 
+function render(library) {
+  
+  for (let i = 0; i < library.length; i++) {
+    const book = document.createElement('div');
+    book.classList.add('book');
 
-let tag_id = document.getElementById('books');
-tag_id.innerHTML('HTML string');
+    let bookTitle = document.createElement('div');
+    bookTitle.classList.add('book-title');
+    bookTitle.innerHTML = library[i].title;
+    book.appendChild(bookTitle);
 
-// const book1 = new Book('La Odisea', 'anonymus', 243, 'not read')
-// console.log(book1.info());
+    let bookAuthor = document.createElement('div');
+    bookAuthor.classList.add('book-author');
+    bookAuthor.innerHTML = library[i].author;
+    book.appendChild(bookAuthor);
+
+    let bookPages = document.createElement('div');
+    bookPages.classList.add('book-pages');
+    bookPages.innerHTML = library[i].pages;
+    book.appendChild(bookPages);
+
+    let bookRead = document.createElement('div');
+    bookRead.classList.add('book-read');
+    if (library[i].read === true) {
+      bookRead.innerHTML = 'Read';
+    } else {
+      bookRead.innerHTML = "didn't read yet";
+    }
+    bookRead.innerHTML = library[i].read;
+    book.appendChild(bookRead);
+
+    const libraryList = document.querySelector('.library-list');
+    libraryList.appendChild(book);
+  }
+}
+
+// let tag_id = document.getElementById('books');
+// tag_id.innerHTML('HTML string');
+
+const book1 = new Book('La Odisea', 'anonymus', 243, true);
+const book2 = new Book('Test book', 'Test author', 501, false);
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+render(myLibrary);
